cab82b0a12467ef713084770a2d111e8e345f648
DawidABarcik <77282347+DawidABarcik@users.noreply.github.com>
2021-03-10 15:57:53 +0000

    Update 2i_Test.java

--- 2i_Test.java
+++ 2i_Test.java
@@ -2,9 +2,7 @@ public class Iteration {
     public static void main(String args[]) 
     {
         int a,b,c,x,y,z;
-        int start = 1;
-        int current = start;
-        boolean last = false;;
+        int current = 1;
         int i = 0;
         a = 100;
         b = 200;
@@ -13,36 +11,35 @@ public class Iteration {
         y = 8;
         z = 9;
         
-        while (current <= c && last == false)
+        while (current <= c)
         {
             if (current < a)
             {
+                
                 current = i * x;
                 if (current >= a)
                 {
                     current = i * y;
                 }
+                
                 i += 1;
             }
             else if (current > a && current <= b)
             {
+                
                 current = i * y; 
                 
-                if (current >= b)
+                if (current > b)
                 {
                     current = i * z;
                 }
+                
                 i += 1;
             }
             else if (current >= b && current < c)
             {
-                current = i * z;
                 
-                if (current >= c)
-                {
-                    current = ((i -= 1) * z);
-                    last = true;
-                }
+                current = i * z;
                 i += 1;
             }
             System.out.println(current);
